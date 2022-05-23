ee5ab47b903c16bf64fc9d49251bd2b0b7cdeb64
khanshorif331 <khanshorif331@gmail.com>
2022-05-23 20:54:46 +0600

    database connected

--- index.js
+++ index.js
@@ -3,10 +3,29 @@ const app = express()
 const port = process.env.PORT || 5000
 const cors = require('cors')
 require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb')
 
 // middlewares
 app.use(cors())
 app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.85nxi.mongodb.net/?retryWrites=true&w=majority`
+const client = new MongoClient(uri, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	serverApi: ServerApiVersion.v1,
+})
+
+async function run() {
+	try {
+		await client.connect()
+		console.log('Database connected')
+		const productCollection = client.db('manufacturer').collection('products')
+	} finally {
+	}
+}
+
+run().catch(console.dir)
 
 app.get('/', (req, res) => {
 	res.send('Welcome to Manufacturer Website Server')
