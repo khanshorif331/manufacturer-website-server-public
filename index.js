fdb52055256e0acf78cd39cdfb5f82c24202bc43
khanshorif331 <khanshorif331@gmail.com>
2022-05-23 19:05:39 +0600

    created the basic setup

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000
+const cors = require('cors')
+require('dotenv').config()
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+	res.send('Welcome to Manufacturer Website Server')
+})
+app.listen(port, () => {
+	console.log('Listening to port', port)
+})
