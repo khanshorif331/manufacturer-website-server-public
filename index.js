b483a2169a5b7ee7d1de56b2d961391f2cad8bc6
khanshorif331 <khanshorif331@gmail.com>
2022-05-26 00:00:52 +0600

    delete an user is created

--- index.js
+++ index.js
@@ -38,10 +38,18 @@ async function run() {
 			res.send(result)
 		})
 
+		// delete order
 		app.delete('/myOrder/:id', async (req, res) => {
 			const id = req.params.id
 			const query = { _id: ObjectId(id) }
 			const result = await orderCollection.deleteOne(query)
+			res.send(result)
+		})
+		// delete user
+		app.delete('/user/:id', async (req, res) => {
+			const id = req.params.id
+			const query = { _id: ObjectId(id) }
+			const result = await userCollection.deleteOne(query)
 			res.send(result)
 		})
 
