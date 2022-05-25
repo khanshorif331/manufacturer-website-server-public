a3b46420fc2dbc53265e0c09bf5458ec82ab6f26
khanshorif331 <khanshorif331@gmail.com>
2022-05-26 03:38:50 +0600

    product delete api created

--- index.js
+++ index.js
@@ -53,6 +53,14 @@ async function run() {
 			res.send(result)
 		})
 
+		// delete product
+		app.delete('/product/:id', async (req, res) => {
+			const id = req.params.id
+			const query = { _id: ObjectId(id) }
+			const result = await productCollection.deleteOne(query)
+			res.send(result)
+		})
+
 		// all users
 		app.get('/users', async (req, res) => {
 			const query = {}
