b78d8107e4541a80456336fab817ba44bb5b6aac
khanshorif331 <khanshorif331@gmail.com>
2022-05-28 02:51:18 +0600

    created route to check admin

--- index.js
+++ index.js
@@ -55,6 +55,14 @@ async function run() {
 			res.send({ clientSecret: paymentIntent.client_secret })
 		})
 
+		// verifying the admin
+		app.get('/admin/:email', async (req, res) => {
+			const email = req.params.email
+			const user = await userCollection.findOne({ email: email })
+			const isAdmin = user.role === 'admin'
+			res.send({ admin: isAdmin })
+		})
+
 		// delete order
 		app.delete('/myOrder/:id', async (req, res) => {
 			const id = req.params.id
